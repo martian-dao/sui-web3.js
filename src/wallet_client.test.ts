22a763fc27020ec3b6e0a1873e7bfffbdbe155ca
MartianAbhishek <abhishek@martiandao.xyz>
2022-12-01 14:42:48 +0530

    removed user account from simulate txn

--- src/wallet_client.test.ts
+++ src/wallet_client.test.ts
@@ -1,8 +1,12 @@
 import { WalletClient } from "./wallet_client";
 import * as Nacl from "tweetnacl";
+import { SignableTransaction } from "./signers/txn-data-serializers/txn-data-serializer";
 
 const NODE_URL = 'https://fullnode.devnet.sui.io/';
 const FAUCET_URL = 'https://faucet.devnet.sui.io/gas';
+
+let alice;
+let aliceAccount;
 
 const apis = new WalletClient(NODE_URL, FAUCET_URL);
 
@@ -27,10 +31,34 @@ function verifySignature(message, signature, publicKey) {
   );
 }
 
-test("verify create wallet", async () => {
-  const alice = await apis.createWallet();
-  const aliceAccount = await WalletClient.getAccountFromMetaData(
-    alice.code,
+async function setupAccount() {
+  if(alice) return;
+  try{
+    alice = await apis.createWallet();
+    aliceAccount = await WalletClient.getAccountFromMetaData(
+      alice.code,
+    );
+    await apis.airdrop(aliceAccount.getPublicKey().toSuiAddress());
+  }catch(err){
+    const mnemonic = "arena nothing skate then sport huge fence era cheese client powder tackle"
+    aliceAccount = await WalletClient.getAccountFromMetaData(mnemonic)
+  }
+}
+
+// to deal with faucet rate limit
+beforeEach(async () => {
+  await setupAccount();
+});
+
+test("verify create wallet and airdrop", async () => {
+  const balance = await apis.getBalance(aliceAccount.getPublicKey().toSuiAddress())
+  expect(balance).toBe(
+    10000000n || 50000000n
   );
-  console.log(aliceAccount.toPrivateKeyObject())
+});
+
+test("verify simulation wallet", async () => {
+  const txn: SignableTransaction = { "kind": "moveCall", "data": { "packageObjectId": "0x2", "module": "devnet_nft", "function": "mint", "typeArguments": [], "arguments": ["Example NFT Dapps", "An NFT created by Sui Wallet", "https://aptos.dev/img/nyan.jpeg"], "gasBudget": 10000 } }
+  const response = await apis.simulateTransaction(aliceAccount.getPublicKey().toSuiAddress(), txn);
+  expect(response.status.status).toBe('success')
 });
