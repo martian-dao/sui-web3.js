829a2dc6295d1a9a0675fa0fb0ec2b8c88ecb915
bruh <nkhan054002@gmail.com>
2023-08-28 17:12:23 +0530

    uncommented prev tests

--- src/wallet-client.test.ts
+++ src/wallet-client.test.ts
@@ -29,15 +29,6 @@ function hexToUint8Array(hexString: string) {
 
 const apis = new WalletClient(NODE_URL, FAUCET_URL);
 
-function allEntriesMatch(arr, arr2) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr2[i]) {
-      return false; // If any entry doesn't match, return false
-    }
-  }
-  return true; // All entries matched
-}
-
 /**
  *
  * @param metadata: string which is signed by private key
@@ -60,17 +51,17 @@ function verifySignature(message, signature, publicKey) {
 }
 
 async function setupAccount() {
-  // if (alice) return;
-  // try {
-  //   alice = await apis.createWallet();
-  //   aliceAccount = await WalletClient.fromDerivePath(alice.code);
-  //   await apis.airdrop(aliceAccount.getPublicKey().toSuiAddress());
-  // } catch (err) {
-  //   const mnemonic =
-  //     'enhance nephew render claim tube grace dream cheese coyote sad relief broom';
-  //   alice = await apis.importWallet(mnemonic);
-  //   aliceAccount = await WalletClient.fromDerivePath(mnemonic);
-  // }
+  if (alice) return;
+  try {
+    alice = await apis.createWallet();
+    aliceAccount = await WalletClient.fromDerivePath(alice.code);
+    await apis.airdrop(aliceAccount.getPublicKey().toSuiAddress());
+  } catch (err) {
+    const mnemonic =
+      'enhance nephew render claim tube grace dream cheese coyote sad relief broom';
+    alice = await apis.importWallet(mnemonic);
+    aliceAccount = await WalletClient.fromDerivePath(mnemonic);
+  }
 }
 
 // to deal with faucet rate limit
@@ -78,121 +69,119 @@ beforeEach(async () => {
   await setupAccount();
 });
 
-//I HAve commneted the below part 
+test('verify create wallet and airdrop', async () => {
+  const balance = await apis.getBalance(
+    aliceAccount.getPublicKey().toSuiAddress(),
+  );
+  expect(parseInt(balance)).toBeGreaterThanOrEqual(0);
+});
 
-// test('verify create wallet and airdrop', async () => {
-//   const balance = await apis.getBalance(
-//     aliceAccount.getPublicKey().toSuiAddress(),
-//   );
-//   expect(parseInt(balance)).toBeGreaterThanOrEqual(0);
-// });
+test('verify Adding accounts', async () => {
+  const response = await apis.createNewAccount(alice.code, 1);
+  expect(response.derivationPath).toBe("m/44'/784'/1'/0'/0'");
+});
 
-// test('verify Adding accounts', async () => {
-//   const response = await apis.createNewAccount(alice.code, 1);
-//   expect(response.derivationPath).toBe("m/44'/784'/1'/0'/0'");
-// });
+test('verify Import Wallet', async () => {
+  const response = await apis.importWallet(
+    'enhance nephew render claim tube grace dream cheese coyote sad relief broom',
+  );
+  expect(response.accounts.length).toBe(1);
+});
 
-// test('verify Import Wallet', async () => {
-//   const response = await apis.importWallet(
-//     'enhance nephew render claim tube grace dream cheese coyote sad relief broom',
-//   );
-//   expect(response.accounts.length).toBe(1);
-// });
+test('verify transferSui', async () => {
+  const bobAccount = await apis.createWallet();
+  const txnHash = await apis.transferSui(
+    1,
+    aliceAccount,
+    bobAccount.accounts[0].address,
+  );
+  const balance = await apis.getBalance(
+    bobAccount.accounts[0].address,
+    SUI_TYPE_ARG,
+  );
+  expect(balance).toBe('1');
+});
 
-// test('verify transferSui', async () => {
-//   const bobAccount = await apis.createWallet();
-//   const txnHash = await apis.transferSui(
-//     1,
-//     aliceAccount,
-//     bobAccount.accounts[0].address,
-//   );
-//   const balance = await apis.getBalance(
-//     bobAccount.accounts[0].address,
-//     SUI_TYPE_ARG,
-//   );
-//   expect(balance).toBe('1');
-// });
+test('verify getAllBalances', async () => {
+  const coins = await apis.getAllBalances(
+    aliceAccount.getPublicKey().toSuiAddress(),
+  );
+  expect(coins.length).toBeGreaterThan(0);
+});
 
-// test('verify getAllBalances', async () => {
-//   const coins = await apis.getAllBalances(
-//     aliceAccount.getPublicKey().toSuiAddress(),
-//   );
-//   expect(coins.length).toBeGreaterThan(0);
-// });
+test('verify getCoinsWithRequiredBalance', async () => {
+  // TODO: setup test
+});
 
-// test('verify getCoinsWithRequiredBalance', async () => {
-//   // TODO: setup test
-// });
+test('verify getCoins', async () => {
+  const coins = await apis.getCoins(
+    SUI_TYPE_ARG,
+    aliceAccount.getPublicKey().toSuiAddress(),
+  );
+  expect(coins.length).toBeGreaterThan(0);
+});
 
-// test('verify getCoins', async () => {
-//   const coins = await apis.getCoins(
-//     SUI_TYPE_ARG,
-//     aliceAccount.getPublicKey().toSuiAddress(),
-//   );
-//   expect(coins.length).toBeGreaterThan(0);
-// });
+test('verify getStake', async () => {
+  const stakes = await apis.getStake(
+    aliceAccount.getPublicKey().toSuiAddress(),
+  );
+  expect(stakes.length).toBe(0);
+});
 
-// test('verify getStake', async () => {
-//   const stakes = await apis.getStake(
-//     aliceAccount.getPublicKey().toSuiAddress(),
-//   );
-//   expect(stakes.length).toBe(0);
-// });
+test('verify simulateTransaction', async () => {
+  const bobAccount = await apis.createWallet();
+  const txn = new TransactionBlock();
+  const coin = txn.splitCoins(txn.gas, [txn.pure(1)]);
 
-// test('verify simulateTransaction', async () => {
-//   const bobAccount = await apis.createWallet();
-//   const txn = new TransactionBlock();
-//   const coin = txn.splitCoins(txn.gas, [txn.pure(1)]);
+  txn.transferObjects([coin], txn.pure(bobAccount.accounts[0].address));
+  txn.setSender(aliceAccount.getPublicKey().toSuiAddress());
 
-//   txn.transferObjects([coin], txn.pure(bobAccount.accounts[0].address));
-//   txn.setSender(aliceAccount.getPublicKey().toSuiAddress());
+  const txnBytes = await txn.build({ provider: apis.provider });
+  const simulationResp = await apis.simulateTransaction(txnBytes);
 
-//   const txnBytes = await txn.build({ provider: apis.provider });
-//   const simulationResp = await apis.simulateTransaction(txnBytes);
+  expect(simulationResp.effects.status.status).toBe('success');
+});
 
-//   expect(simulationResp.effects.status.status).toBe('success');
-// });
+test('verify getTransactions', async () => {
+  const transactions = await apis.getTransactions(
+    aliceAccount.getPublicKey().toSuiAddress(),
+  );
+  expect(transactions.length).toBeGreaterThan(0);
+});
 
-// test('verify getTransactions', async () => {
-//   const transactions = await apis.getTransactions(
-//     aliceAccount.getPublicKey().toSuiAddress(),
-//   );
-//   expect(transactions.length).toBeGreaterThan(0);
-// });
+test('verify getNfts', async () => {
+  // This address has kiosk nfts
+  const nfts = await apis.getNfts(
+    '0x1ba6558d33b2b2eac213a06443e51b16c6a1fe9f4e9ad09c96ddeafb1d6fc173',
+  );
+  // console.log(nfts);
+  expect(1).toBeGreaterThan(0);
+});
 
-// test('verify getNfts', async () => {
-//   // This address has kiosk nfts
-//   const nfts = await apis.getNfts(
-//     '0x1ba6558d33b2b2eac213a06443e51b16c6a1fe9f4e9ad09c96ddeafb1d6fc173',
-//   );
-//   // console.log(nfts);
-//   expect(1).toBeGreaterThan(0);
-// });
+test('verify getAccountFromMnemonic', async () => {
+  const account = WalletClient.getAccountFromMnemonic(
+    'enhance nephew render claim tube grace dream cheese coyote sad relief broom',
+  );
+  expect(account.getPublicKey().toSuiAddress()).toBe(
+    '0xb7657ece8328ed50a93a72aa091e9686cbd272ef584cba2f259452999f7c5ab1',
+  );
+});
 
-// test('verify getAccountFromMnemonic', async () => {
-//   const account = WalletClient.getAccountFromMnemonic(
-//     'enhance nephew render claim tube grace dream cheese coyote sad relief broom',
-//   );
-//   expect(account.getPublicKey().toSuiAddress()).toBe(
-//     '0xb7657ece8328ed50a93a72aa091e9686cbd272ef584cba2f259452999f7c5ab1',
-//   );
-// });
-
-// test('verify toPrivateKeyObject', async () => {
-//   const account = WalletClient.getAccountFromMnemonic(
-//     'enhance nephew render claim tube grace dream cheese coyote sad relief broom',
-//   );
-//   const { address, publicKeyHex, privateKeyHex } = account.toPrivateKeyObject();
-//   expect(address).toBe(
-//     '0xb7657ece8328ed50a93a72aa091e9686cbd272ef584cba2f259452999f7c5ab1',
-//   );
-//   expect(publicKeyHex).toBe(
-//     '0x5419d348ed7ddfcf57dae1403b0d98b8a28d5e572e3e518d41d8a9d107aa0dde',
-//   );
-//   expect(privateKeyHex).toBe(
-//     '0xe8ab49ef10bedee4183d3c7c0149be2003c5867b2b41f1cbdd3875148d899819',
-//   );
-// });
+test('verify toPrivateKeyObject', async () => {
+  const account = WalletClient.getAccountFromMnemonic(
+    'enhance nephew render claim tube grace dream cheese coyote sad relief broom',
+  );
+  const { address, publicKeyHex, privateKeyHex } = account.toPrivateKeyObject();
+  expect(address).toBe(
+    '0xb7657ece8328ed50a93a72aa091e9686cbd272ef584cba2f259452999f7c5ab1',
+  );
+  expect(publicKeyHex).toBe(
+    '0x5419d348ed7ddfcf57dae1403b0d98b8a28d5e572e3e518d41d8a9d107aa0dde',
+  );
+  expect(privateKeyHex).toBe(
+    '0xe8ab49ef10bedee4183d3c7c0149be2003c5867b2b41f1cbdd3875148d899819',
+  );
+});
 
 test('Verifying MPC transferSui', async () => {
   //keygen
